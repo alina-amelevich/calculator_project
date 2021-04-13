cd538aaa7fd16855f960f41f6e2c23de42f39a32
Alina <alina@Apples-MBP.lan>
2021-04-13 21:10:09 +0300

    Started work with events

--- calc.js
+++ calc.js
@@ -1,1 +1,16 @@
 'use strict'
+
+let calcContainer = document.querySelector('#calc-container');
+
+calcContainer.addEventListener('click', calculation);
+
+function calculation(eventObject) {
+    eventObject = eventObject || window.event;
+    let target = eventObject.target // Где был клик?
+
+    let targetValue = target.getAttribute('data-button');
+    if (!targetValue) return; // Если клик был не по кнопке - вып-ние ф-ции прерывается
+    
+    console.log('Button was clicked'); // Эт временно для проверки, отладки
+};
+
